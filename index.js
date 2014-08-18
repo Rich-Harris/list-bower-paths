ca390e15bb0e6973aae899d238888c55822b20bc
Rich Harris <richard.a.harris@gmail.com>
2014-08-18 11:37:14 -0400

    add noext and directory options, fix a bug or two

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ var fs = require( 'fs' ),
 	findup = require( 'findup-sync' ),
 	glob = promo( require( 'glob' ) ),
 
-	readFile = promo( fs.readFile );
+	readFile = promo( fs.readFile ),
+
+	bower_components;
 
 var listBowerPaths = function ( options, cb ) {
 	var cwd, bowerrc, bower_components, paths, promise;
 
-	options = options || {};
+	if ( typeof options === 'function' ) {
+		cb = options;
+		options = {};
+	} else if ( !options ) {
+		options = {};
+	}
 
 	cwd = options.cwd || process.cwd();
 	bowerrc = findup( '.bowerrc', { cwd: cwd });
@@ -20,60 +27,80 @@ var listBowerPaths = function ( options, cb ) {
 	}
 
 	if ( options.sync ) {
-		return doSync( cwd, bowerrc, options.relative );
+		bower_components = options.directory || ( bowerrc && JSON.parse( fs.readFileSync( bowerrc ).toString() ).directory ) || 'bower_components';
+		return doSync( cwd, bower_components, options );
 	}
 
-	promise = doAsync( cwd, bowerrc, options.relative );
-	if ( cb ) promise.then( cb );
+	if ( options.directory || !bowerrc ) {
+		bower_components = options.directory || 'bower_components';
+		promise = doAsync( cwd, bower_components, options );
+	} else {
+		promise = readFile( bowerrc ).then( function ( json ) {
+			bower_components = JSON.parse( json ).directory || 'bower_components';
+			return doAsync( cwd, bower_components, options );
+		});
+	}
+
+	if ( cb ) {
+		promise.then( function ( result ) {
+			cb( null, result );
+		}, cb );
+	}
 
 	return promise;
 };
 
-function doSync ( cwd, bowerrc, relative ) {
-	var bower_components, paths;
-
-	bower_components = path.join( cwd, JSON.parse( fs.readFileSync( bowerrc ).toString() ).directory || 'bower_components' );
-	paths = {};
+function doSync ( cwd, bower_components, options ) {
+	var paths = {};
 
 	glob.sync( path.join( bower_components, '*/.bower.json' ) ).forEach( function ( bowerFile ) {
-		var dir, name, json, config, main;
+		var dir, name, json, config, main, modulePath;
 
-		dir = path.dirname( bowerFile ).replace( ( relative ? bower_components : cwd ) + '/', '' );
+		dir = path.dirname( bowerFile ).replace( ( options.relative ? bower_components : cwd ) + '/', '' );
 
 		json = fs.readFileSync( bowerFile ).toString();
 		config = JSON.parse( json );
 		main = config.main;
 
 		if ( main ) {
-			paths[ config.name ] = path.join( dir, main );
+			modulePath = path.join( dir, main );
+
+			if ( options.noext ) {
+				modulePath = modulePath.replace( /\.js$/, '' );
+			}
+
+			paths[ config.name ] = modulePath;
 		}
 	});
 
 	return paths;
 }
 
-function doAsync ( cwd, bowerrc, base, cb ) {
+function doAsync ( cwd, bower_components, options ) {
 	return readFile( bowerrc ).then( function ( result ) {
-		var bower_components, paths;
-
-		bower_components = path.join( cwd, JSON.parse( result.toString() ).directory || 'bower_components' );
-		paths = {};
+		var paths = {};
 
 		return glob( path.join( bower_components, '*/.bower.json' ) ).then( function ( files ) {
 			var promises;
 
 			promises = files.map( function ( bowerFile ) {
 				return readFile( bowerFile ).then( function ( result ) {
-					var dir, name, json, config, main;
+					var dir, name, json, config, main, modulePath;
 
-					dir = path.dirname( bowerFile ).replace( ( relative ? bower_components : cwd ) + '/', '' );
+					dir = path.dirname( bowerFile ).replace( ( options.relative ? bower_components : cwd ) + '/', '' );
 
 					json = result.toString();
 					config = JSON.parse( json );
 					main = config.main;
 
 					if ( main ) {
-						paths[ config.name ] = path.join( dir, main );
+						modulePath = path.join( dir, main );
+
+						if ( options.noext ) {
+							modulePath = modulePath.replace( /\.js$/, '' );
+						}
+
+						paths[ config.name ] = modulePath;
 					}
 				});
 			});
